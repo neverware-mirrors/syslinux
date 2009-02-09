3d2c855d1ebcca44dd3318020fbc054ab1830cca
H. Peter Anvin <hpa@zytor.com>
2009-02-09 11:21:43 +0100

    extlinux.asm: update copyright header

--- core/extlinux.asm
+++ core/extlinux.asm
@@ -5,12 +5,12 @@
 ;
 ;  A program to boot Linux kernels off an ext2/ext3 filesystem.
 ;
-;   Copyright 1994-2008 H. Peter Anvin - All Rights Reserved
+;   Copyright 1994-2009 H. Peter Anvin - All Rights Reserved
 ;
 ;  This program is free software; you can redistribute it and/or modify
 ;  it under the terms of the GNU General Public License as published by
-;  the Free Software Foundation, Inc., 53 Temple Place Ste 330,
-;  Boston MA 02111-1307, USA; either version 2 of the License, or
+;  the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+;  Boston MA 02110-1301, USA; either version 2 of the License, or
 ;  (at your option) any later version; incorporated herein by reference.
 ;
 ; ****************************************************************************
