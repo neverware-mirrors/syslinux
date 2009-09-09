408e9890f28abc5f204e7cd3643e6417890e628d
H. Peter Anvin <hpa@zytor.com>
2009-09-08 21:05:53 -0700

    core: declare jiffies_t, MS_PER_JIFFY
    Create a specific type for jiffies, and an MS_PER_JIFFY macro.
    
    Signed-off-by: H. Peter Anvin <hpa@zytor.com>

--- com32/include/core/jiffies.h
+++ com32/include/core/jiffies.h
@@ -4,10 +4,14 @@
 #ifndef _CORE_JIFFIES_H
 #define _CORE_JIFFIES_H
 
-#define HZ	18
+#define HZ		18
+#define MS_PER_JIFFY	55
 
-extern const volatile uint32_t __jiffies;
-static inline uint32_t jiffies(void)
+typedef uint32_t jiffies_t;
+typedef int32_t  sjiffies_t;
+
+extern const volatile jiffies_t __jiffies;
+static inline jiffies_t jiffies(void)
 {
     return __jiffies;
 }
